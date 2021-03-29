8a45040fab649f2924b1d367ab84b570a25b147e
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-29 17:35:10 +0530

    Added: wage per hr and Full Day hr

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,14 +5,21 @@ public class EmpWageBuilder
 		System.out.println("Welcome to the Employee Wage Computation");
 			//constants
 			int FULL_TIME=1;
+			int EMP_RATE_PER_HOUR=20;
+			// Variables
+			int empHrs=0;
+			int empWage=0;
 			//Computation
 				double empCheck=Math.floor(Math.random()*10)%2;
 				if(empCheck==FULL_TIME)
 				{
-					System.out.println("Employee is present");
+						empHrs=8;
 				}
-					else
-					System.out.println("EMployee is Absent");
-
+				else
+				{
+						empHrs=0;
+				}
+				empWage=empHrs*EMP_RATE_PER_HOUR;
+				System.out.println("EMp Wage: "+empWage);
 	}
 }
