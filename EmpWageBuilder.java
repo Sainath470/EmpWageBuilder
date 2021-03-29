58d9582b86341be9a718afad38087b805f967405
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-29 18:32:08 +0530

    Added: method for computing total empWage

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -6,7 +6,7 @@ public class EmpWageBuilder
 				public static final int EMP_RATE_PER_HOUR=20;
 				public static final int NUM_OF_WORKING_DAYS=20;
 				public static final int MAX_HRS_IN_MONTH=100;
-	 public static void main(String args[])
+	 public static int computeEmpWage()
   	 {
 
 				// Variables
@@ -36,5 +36,11 @@ public class EmpWageBuilder
 		}
 	   totalEmpWage=totalEmpHrs*EMP_RATE_PER_HOUR;
 		System.out.println("Total Emp Wage: " +totalEmpWage);
+		return totalEmpWage;
 	 }
+
+		public static void main(String[] args)
+		{
+			computeEmpWage();
+		}
 }
