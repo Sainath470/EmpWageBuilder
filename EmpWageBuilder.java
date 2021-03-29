dfd652e9dc23576359ee244cec7b88feea63de46
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-29 17:26:55 +0530

    Added: Employee is present or not

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -3,5 +3,16 @@ public class EmpWageBuilder
 	public static void main(String args[])
 	{
 		System.out.println("Welcome to the Employee Wage Computation");
+			//constants
+			int FULL_TIME=1;
+			//Computation
+				double empCheck=Math.floor(Math.random()*10)%2;
+				if(empCheck==FULL_TIME)
+				{
+					System.out.println("Employee is present");
+				}
+					else
+					System.out.println("EMployee is Absent");
+
 	}
 }
