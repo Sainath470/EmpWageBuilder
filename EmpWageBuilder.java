a3c94ddc39fe4dcf41b8d18fbcef3b033bbc91d0
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-29 18:03:28 +0530

    Added:total wage for a month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -4,13 +4,15 @@ public class EmpWageBuilder
 				public static final int PART_TIME=1;
 				public static final int FULL_TIME=2;
 				public static final int EMP_RATE_PER_HOUR=20;
+				public static final int NUM_OF_WORKING_DAYS=20;
 	 public static void main(String args[])
   	 {
 
 				// Variables
-				int empHrs=0;
-				int empWage=0;
+				int empHrs=0,empWage=0,totalEmpWage=0;
 				//Computation
+		for( int day=0; day<NUM_OF_WORKING_DAYS; day++)
+		{
 				int empCheck=(int)Math.floor(Math.random()*10)%3;
 				switch(empCheck)
 				{
@@ -25,7 +27,11 @@ public class EmpWageBuilder
 									empHrs=0;
 									break;
 				}
+
 				empWage=empHrs*EMP_RATE_PER_HOUR;
 				System.out.println("Emp Wage: "+empWage);
+				totalEmpWage+=empWage;
+		}
+		System.out.println("Total Emp Wage: " +totalEmpWage);
 	 }
 }
